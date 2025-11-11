939fab471087d33f7d1442c7d17aceac0ca8b8a9
ST10489347 <ST10489347@vcconnect.edu.za>
2025-11-11 15:52:43 +0200

    Update App.tsx with new changes

---
+++ App.tsx
@@ -0,0 +1,6 @@
+//Use of app.tsx to be Navigation Hub 
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+//screen imports 
