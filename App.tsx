a2129f45fc6964540b31204c7e322100296482fb
ST10489347 <ST10489347@vcconnect.edu.za>
2025-11-11 15:58:21 +0200

    Used App.tsx as Navigation hub for Navigation

--- App.tsx
+++ App.tsx
@@ -4,3 +4,33 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 //screen imports 
+import HomeScreen from './screens/HomeScreen'
+import ChefScreen from "./screens/ChefScreen";
+import FilterScreen from './screens/FilterScreen'
+
+export type RootStackParamList = {
+  Home: undefined;
+  Chef: undefined;
+  Filter: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function App() {
+  return (
+<NavigationContainer>
+  <Stack.Navigator
+  initialRouteName="Home" 
+  screenOptions={{
+ headerStyle: { backgroundColor: "#80deea"},
+ headerTintColor: "#fff",
+ headerTitleStyle: {fontWeight:"bold"},
+  }}>
+
+  <Stack.Screen name="Home" component={HomeScreen} options={{title:"Menu" Overview}} />
+  <Stack.Screen name="Chef" component={ChefScreen} options={{title: "Add Menu Item"}}/>
+  <Stack.Screen name="Filter" component={FilterScreen} options={{title: "Filter Menu"}} />
+  </Stack.Navigator>
+</NavigationContainer>
+  );
+}
